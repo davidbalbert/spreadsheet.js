f905c57bd86eb54c2b3b4dbff68dc7b6aa47a103
David Albert <davidbalbert@gmail.com>
2018-11-23 11:52:54 -0500

    Build AST in advance of CellRef support

--- src/parser.js
+++ src/parser.js
@@ -40,48 +40,106 @@ Formula {
 
 const formulaGrammar = ohm.grammar(GRAMMAR_SRC);
 
-const formulaSemantics = formulaGrammar.createSemantics().addOperation('eval', {
+class UnOp {
+  constructor(child) {
+    this.child = child;
+  }
+}
+
+class BinOp {
+  constructor(left, right) {
+    this.left = left;
+    this.right = right;
+  }
+}
+
+class Plus extends BinOp {
+  eval() {
+    return this.left.eval() + this.right.eval();
+  }
+}
+
+class Minus extends BinOp {
+  eval() {
+    return this.left.eval() - this.right.eval();
+  }
+}
+
+class Times extends BinOp {
+  eval() {
+    return this.left.eval() * this.right.eval();
+  }
+}
+
+class Divide extends BinOp {
+  eval() {
+    return this.left.eval() / this.right.eval();
+  }
+}
+
+class Pow extends BinOp {
+  eval() {
+    return this.left.eval() ** this.right.eval();
+  }
+}
+
+class Neg extends UnOp {
+  eval() {
+    return -1 * this.child.eval();
+  }
+}
+
+class Num extends UnOp {
+  eval() {
+    return this.child;
+  }
+}
+
+class CellRef extends UnOp {
+  eval() {
+    throw 'not implemented';
+  }
+}
+
+const formulaSemantics = formulaGrammar.createSemantics().addOperation('toAst', {
   Formula: function(eq, e) {
-    return e.eval();
+    return e.toAst();
   },
   Exp: function(e) {
-    return e.eval();
-  },
-  AddExp: function(e) {
-    return e.eval();
+    return e.toAst();
   },
   AddExp_plus: function(left, op, right) {
-    return left.eval() + right.eval();
+    return new Plus(left.toAst(), right.toAst());
   },
   AddExp_minus: function(left, op, right) {
-    return left.eval() - right.eval();
+    return new Minus(left.toAst(), right.toAst());
   },
   MulExp_times: function(left, op, right) {
-    return left.eval() * right.eval();
+    return new Times(left.toAst(), right.toAst());
   },
   MulExp_divide: function(left, op, right) {
-    return left.eval() / right.eval();
+    return new Divide(left.toAst(), right.toAst());
   },
   ExpExp_power: function(left, op, right) {
-    return left.eval() ** right.eval();
+    return new Pow(left.toAst(), right.toAst());
   },
   PriExp: function(e) {
-    return e.eval();
+    return e.toAst();
   },
   PriExp_paren: function(open, exp, close) {
-    return exp.eval();
+    return exp.toAst();
   },
   PriExp_neg: function(op, e) {
-    return -1 * e.eval();
+    return new Neg(e.toAst());
   },
   PriExp_pos: function(op, e) {
-    return e.eval();
+    return e.toAst();
   },
   number: function(chars) {
-    return parseFloat(this.sourceString);
+    return new Num(parseFloat(this.sourceString));
   },
   cell: function(col, row) {
-    return this.sourceString;
+    return new CellRef(this.sourceString);
   }
 });
 
@@ -107,7 +165,7 @@ class Formula {
 
   eval() {
     if (this.match.succeeded()) {
-      return formulaSemantics(this.match).eval();
+      return formulaSemantics(this.match).toAst().eval();
     } else {
       return "#ERROR!";
     }
